541b4ac9e652e8591d19944b282514975bc6703e
NPoulin02 <npoulin02@gmail.com>
2014-11-25 17:05:29 -0500

    Update rock-paper-cisors.py

--- rock-paper-cisors.py
+++ rock-paper-cisors.py
@@ -3,32 +3,22 @@ from random import randrange
 restart = True
 userChoice = 0
 cpuChoice = 0
+choice = ['rock', 'paper', 'cisors']
 
 while restart == True:
 	while userChoice != 1 and userChoice != 2 and userChoice != 3:
-	  try:
+		try:
 			userChoice = int(input('Roc, paper or cisors ?\n1 - Roc\n2 - Paper\n3 - Cisors\n'))
 		except:
-			print('Invalid value, please choose a valid value.')
+			print('Invalid value, please enter a valid value.')
 			continue
 		if userChoice != 1 and userChoice != 2 and userChoice != 3:
-			print('Invalid value, please choose a valid value.')
+			print('Invalid value, please enter a valid value.')
 
 	cpuChoice = randrange(1, 4)
 
-	if userChoice == 1:
-		print('You choosed the rock')
-	elif userChoice == 2:
-		print('You choosed the paper')
-	else:
-		print('You choosed the cisors')
-
-	if cpuChoice == 1:
-		print('The computer choosed the rock')
-	elif cpuChoice == 2:
-		print('The computer choosed the paper')
-	else:
-		print('The computer choosed the cisors')
+	print('You choosed the ' + choice[userChoice - 1])
+	print('The computer choosed the ' + choice[cpuChoice - 1])
 
 	if userChoice == 1:
 		if cpuChoice == 3:
@@ -58,7 +48,7 @@ while restart == True:
 	while restart != 'Y' and restart != 'y' and restart != 'N' and restart != 'n':
 		restart = input('Would you like to restart? Y/N\n')
 		if restart != 'Y' and restart != 'y' and restart != 'N' and restart != 'n':
-			print('Invalid value, please choose a valid value.')
+			print('Invalid value, please enter a valid value.')
 
 	if restart == 'Y' or restart == 'y':
 		restart = True
